503ec8e055a0c9924dae193eed8ed7e362ab8598
AlexKri72 <krial11@rambler.ru>
2022-01-29 21:37:38 +0500

    Example 005 If Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите Ваше имя: ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "маша")
+{
+    
+    Console.Write("Ура, это же ");
+    Console.Write(userName);
+    Console.WriteLine(" !");
+}
+    else
+    {
+        Console.Write("Привет, ");
+        Console.Write(userName);
+        Console.Write(" !");
+    }
+
