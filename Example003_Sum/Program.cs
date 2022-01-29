7b4ca445e7fb47c3700bb62796c96568cb94910b
AlexKri72 <krial11@rambler.ru>
2022-01-29 20:43:44 +0500

    Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA =3;
+int numberB =5;
+Console.Write ("Первое число равно: ");
+Console.WriteLine (numberA);
+Console.Write ("Второе число равно: ");
+Console.WriteLine (numberB);
+Console.Write ("Сумма двух чисел равна: ");
+Console.Write (numberA+numberB);
